76a02e69407ff586fd5727fa1c88e4cecac03c85
huynhthanhdan1710 <htdan2k@gmail.com>
2023-09-21 14:48:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if(empty($_SESSION['id']))
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.php" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">Admin</span>
+        <span class="d-none d-lg-block">Danht</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
